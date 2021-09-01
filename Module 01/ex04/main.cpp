aca14164b737e636a0d25a8d26b1ba97a8d3e506
sthrace <inbox.d299@gmail.com>
2021-09-01 12:50:13 +0300

    commit

--- Module 01/ex04/main.cpp
+++ Module 01/ex04/main.cpp
@@ -1,42 +1,63 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <fstream>
+#include "header.hpp"
 
-# define DEFAULT "\033[0m"
-# define BOLDRED     "\033[1m\033[31m"
-# define BOLDGREEN   "\033[1m\033[32m"
-# define BOLDYELLOW  "\033[1m\033[33m"
+void	replace(std::ifstream *fsRD, std::ofstream *fdWR, std::string s1, std::string s2)
+{
+	std::stringstream strStream;
+	int	pos = 0, found;
+
+	strStream << fsRD->rdbuf();
+	std::string content = strStream.str();
+	while (1)
+	{
+		if ((found = content.find(s1, pos)) > 0)
+		{
+			content.erase(found, s1.length());
+			content.insert(found, s2);
+			pos = found + s2.length();
+		}
+		else
+			break ;
+	}
+	*fdWR << content << std::endl;
+}
+
+int	open(std::string file, std::string s1, std::string s2)
+{
+	std::ifstream	fsRD(file);
+	if (!fsRD)
+	{
+		std::cout << BOLDRED << "Can not open ";
+		std::cout << BOLDWHITE << "<" << file << ">" << std::endl;
+		return (1);
+	}
+	std::ofstream	fsWR(file.append(".replace"));
+	if (!fsWR)
+	{
+		std::cout << BOLDRED << "Can not open ";
+		std::cout << BOLDWHITE << "<" << file << ">" << std::endl;
+		return (1);
+	}
+	replace(&fsRD, &fsWR, s1, s2);
+	fsRD.close();
+	fsWR.close();
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
 	std::string		file;
 	std::string		s1;
 	std::string		s2;
-	std::string		strInput;
 
 	if (argc != 4)
 	{
 		std::cout << BOLDRED << "Invalid number of parameters" << std::endl;
 		return (1);
 	}
-	
 	file = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
-	std::ifstream	RD(file);
-	if (!RD)
-	{
-		std::cout << BOLDRED;
-		std::cout << "File: " << RD << " can not be opened" << std::endl;
-	}
-	std::ofstream	WR(file.append(".replace"));
-	std::cout << BOLDYELLOW;
-	std::size_t	found 	
-	while (!(getline(RD, strInput).eof()))
-	{
-		std::cout << strInput << std::endl;
-	}
-	std::cout << strInput << std::endl;
+	if (open(file, s1, s2))
+		return (1);
 	return (0);
 }
