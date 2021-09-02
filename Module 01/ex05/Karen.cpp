4b4867c01905af24e741fde7c674c972d5123db3
sthrace <inbox.d299@gmail.com>
2021-09-02 10:46:54 +0300

    commit

--- Module 01/ex05/Karen.cpp
+++ Module 01/ex05/Karen.cpp
@@ -2,24 +2,42 @@
 
 void Karen::complain(std::string level)
 {
+	void (Karen::*levels[4])() = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	switch(std::stoi(level))
+	{
+		case 1 ... 4:
+			(this->*levels[std::stoi(level) - 1])();
+			break ;
+		default:
+			std::cout << BOLDRED;
+			std::cout << "Karen can only complain about 4 things";
+			std::cout << DEFAULT << std::endl;
+			break ;
+	}
 
 }
 
 void Karen::debug(void)
 {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!";
+	std::cout << "I love to get extra bacon for my "
+	"7XL-double-cheese-triple-pickle-special-ketchup "
+	"burger. I just love it!";
 	std::cout << std::endl;
 }
 
 void Karen::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!";
+	std::cout << "I cannot believe adding extra bacon cost "
+	"more money. You don’t put enough! If you did I would "
+	"not have to ask for it!";
 	std::cout << std::endl;
 }
 
 void Karen::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month.";
+	std::cout << "I think I deserve to have some extra bacon "
+	"for free. I’ve been coming here for years and you just "
+	"started working here last month.";
 	std::cout << std::endl;
 }
 
---
+++ Module 01/ex05/main.cpp
@@ -0,0 +1,14 @@
+#include "Karen.hpp"
+
+int	main()
+{
+	Karen	test;
+
+	test.complain("1");
+	test.complain("2");
+	test.complain("3");
+	test.complain("4");
+	test.complain("5");
+	test.complain("0");
+	return (0);
+}
