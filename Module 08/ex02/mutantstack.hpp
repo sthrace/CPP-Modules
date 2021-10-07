55471538a893b517feb67fb4208cfe5be206ed94
sthrace <inbox.d299@gmail.com>
2021-10-07 19:43:25 +0300

    commit

--- Module 08/ex02/mutantstack.hpp
+++ Module 08/ex02/mutantstack.hpp
@@ -4,6 +4,7 @@
 # include <iostream>
 # include <algorithm>
 # include <stack>
+# include <deque>
 
 # define DEFAULT		"\033[0m"
 # define BOLDRED		"\033[1m\033[31m"
@@ -14,10 +15,35 @@
 # define BOLDCYAN		"\033[1m\033[36m"
 # define BOLDWHITE		"\033[1m\033[37m"
 
-class MutantStack
+template<typename T>
+class MutantStack : public std::stack<T>
 {
 	private:
 	public:
+		typedef typename std::deque<T>::iterator 				iterator;
+		typedef typename std::deque<T>::const_iterator  		const_iterator;
+		typedef typename std::deque<T>::reverse_iterator 		reverse_iterator;
+		typedef typename std::deque<T>::const_reverse_iterator  const_reverse_iterator;
+
+		MutantStack(void) : std::stack<T>() {};
+		MutantStack(const MutantStack<T> &source) : std::stack<T>(source) {};
+		~MutantStack() {};
+
+		MutantStack<T>	&operator=(const MutantStack<T> &source) {
+			if (this != &source){
+				this->c = source.c;
+			}
+			return *this;
+		};
+
+		iterator				begin(void) { return MutantStack<T>::c.begin(); };
+		const_iterator			begin(void) const { return MutantStack<T>::c.begin(); };
+		reverse_iterator		rbegin(void) { return MutantStack<T>::c.rbegin(); };
+		const_reverse_iterator	rbegin(void) const { return MutantStack<T>::c.rbegin(); };
+		iterator				end(void) { return MutantStack<T>::c.end(); };
+		const_iterator			end(void) const { return MutantStack<T>::c.end(); };
+		reverse_iterator		rend(void) { return MutantStack<T>::c.rend(); };
+		const_reverse_iterator	rend(void) const { return MutantStack<T>::c.rend(); };
 };
 
 #endif
